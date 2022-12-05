e9795c1a19a6e8a1d8212b0827e8ec45feffb161
MarcoDiFrancesco <marcodifran@gmail.com>
2022-12-05 17:54:55 +0200

    Update run.sh with options

--- project/run.sh
+++ project/run.sh
@@ -1,5 +1,5 @@
 WANDB_MODE=disabled python train.py \
---config_file_name=mountaincarcontinuous_easy \
+--config_file_name=bipedalwalker_easy \
 --agent_name=ddpg \
 --seed=2 \
 --save_video=True \
@@ -10,3 +10,18 @@ WANDB_MODE=disabled python train.py \
 --batch_size 10 \
 --train_episodes 1000 \
 # --testing=True \
+
+# Options:
+# mountaincarcontinuous_easy.yaml
+# 
+# lunarlander_continuous_easy.yaml
+# lunarlander_continuous_medium.yaml
+# lunarlander_discrete_easy.yaml
+# lunarlander_discrete_medium.yaml
+# 
+# bipedalwalker_easy.yaml
+# bipedalwalker_medium.yaml
+# 
+# hopper_easy.yaml
+# hopper_medium.yaml
+# hopper_hard.yaml
